fdaee87233dd2aa1284f0d27120344eb8541b58a
Akram <akrambenaoun1993@gmail.com>
2025-09-22 15:14:59 +0100

    update auth services and create a new login component

--- app/services/authentication.services.ts
+++ app/services/authentication.services.ts
@@ -1,31 +1,58 @@
-const API_URL = "http://localhost:3000/api/auth";
+import axios from "axios";
 
-const login = async (credentials: { email: string; password: string }) => {
-  const response = await fetch(`${API_URL}/login`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(credentials),
-  });
+const backendUrl = process.env.REACT_APP_API_URL as string;
 
-  if (!response.ok) {
-    throw new Error("Invalid credentials");
+if (!backendUrl) {
+  throw new Error("backendUrl is not defined in your .env file");
+}
+
+export interface LoginData {
+  email: string;
+  password: string;
+}
+
+export interface RegisterData {
+  firstName: string, 
+  lastName: string, 
+  email: string, 
+  password: string
+}
+
+const login = async (loginData: LoginData) => {
+  try {
+    // console.log("backendUrl: ", backendUrl);
+    console.log("login data ==: ", loginData);
+    
+    
+    const response = await axios.post(`http://localhost:3000/api/users/login`, loginData);
+    console.log("response from login service: ", response);
+    
+    return response.data;
+  } catch (err: any) {
+    if (err.response?.data?.message) {
+      throw new Error(err.response.data.message);
+    } else {
+      throw new Error("Network or server error");
+    }
   }
-
-  return response.json();
 };
 
-const signup = async (userData: { email: string; password: string }) => {
-  const response = await fetch(`${API_URL}/signup`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(userData),
-  });
-
-  if (!response.ok) {
-    throw new Error("Signup failed");
+const signup = async (registerData: RegisterData) => {
+  try {
+    const response = await axios.post(`http://localhost:3000/api/users/createUser`, registerData);
+    return response.data;
+  } catch (err: any) {
+    if (err.response?.data?.message) {
+      throw new Error(err.response.data.message);
+    } else {
+      throw new Error("Network or server error");
+    }
   }
-
-  return response.json();
 };
 
-export default { login, signup };
+const authService = {
+  login,
+  signup,
+};
+
+export default authService;
