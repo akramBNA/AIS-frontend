3ed4a9135d092c22124a50541042f7687e2196ee
Akram <akrambenaoun1993@gmail.com>
2025-09-22 15:21:18 +0100

    update routes.ts file

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,12 @@
+// import { type RouteConfig, index } from "@react-router/dev/routes";
+
+// export default [index("routes/home.tsx")] satisfies RouteConfig;
+
 import { type RouteConfig, index } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  index("routes/home.tsx"),
+  { path: "/login", file: "routes/login/login.tsx" },
+//   { path: "/signup", file: "routes/signup/signup.tsx" },
+//   { path: "/mainPage", file: "routes/mainPage/mainPage.tsx" },
+] satisfies RouteConfig;
