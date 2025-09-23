c8e04b27fae4a10ac0fae2e86885ef67c2f02c32
Akram <akrambenaoun1993@gmail.com>
2025-09-23 16:47:57 +0100

    re-arrange the routes

--- app/routes.ts
+++ app/routes.ts
@@ -1,13 +1,13 @@
 import { type RouteConfig, index } from "@react-router/dev/routes";
 
 export default [
+  { path: "/login", file: "routes/login/login.tsx" },
+  { path: "/signup", file: "routes/signup/signup.tsx" },
+
   {
-    // parent route with DefaultLayout
     file: "defaultLayout/defaultLayout.tsx",
     children: [
       index("routes/home.tsx"),
-      { path: "/login", file: "routes/login/login.tsx" },
-      { path: "/signup", file: "routes/signup/signup.tsx" },
       { path: "/mainPage", file: "routes/main-page/main-page.tsx" },
       { path: "simulation-input", file: "routes/simulation-input/simulation-input.tsx" },
       { path: "simulations-list", file: "routes/simulations-list/simulations-list.tsx" },
